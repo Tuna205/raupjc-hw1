5dc7a41fd4f193eb91c6c210366c3e7255d411d0
Tuna205 <32743609+Tuna205@users.noreply.github.com>
2017-10-16 00:34:44 +0200

    Add files via upload

---
+++ Pong/Ball.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Pong
+{
+	/// <summary >
+	/// Game ball object representation
+	/// </ summary >
+	public class Ball : Sprite
+	{
+		private float speed = GameConstants.DefaultInitialBallSpeed;
+
+		/// <summary >
+		/// Defines current ball speed in time .
+		/// </ summary >
+		public float Speed
+		{
+			get { return speed; }
+			set
+			{
+				float speed1 = speed;
+				if (value > GameConstants.MaxBallSpeed) speed = GameConstants.MaxBallSpeed;
+				else speed = value;
+			}
+		}
+		public float BumpSpeedIncreaseFactor { get; set; }
+		/// <summary >
+		/// Defines ball direction .
+		/// Valid values ( -1 , -1) , (1 ,1) , (1 , -1) , ( -1 ,1).
+		/// Using Vector2 to simplify game calculation . Potentially
+		/// dangerous because vector 2 can swallow other values as well .
+		/// OPTIONAL TODO : create your own , more suitable type
+		/// </ summary >
+		public Vector2 Direction { get; set; }
+		public Ball(int size, float speed, float defaultBallBumpSpeedIncreaseFactor) : base(size, size)
+		{
+			Speed = speed;
+			BumpSpeedIncreaseFactor = defaultBallBumpSpeedIncreaseFactor;
+			// Initial direction
+			Direction = new Vector2(1, 1);
+		}
+	}
+}
---
+++ Pong/GameConstants.cs
@@ -0,0 +1,17 @@
+﻿namespace Pong
+{
+	public class GameConstants
+	{
+		public const int ScreenHeight = 900;
+		public const int ScreenWidth = 500;
+		public const float PaddleDefaulSpeed = 0.9f;
+		public const int PaddleDefaultWidth = 200;
+		public const int PaddleDefaulHeight = 20;
+		public const float DefaultInitialBallSpeed = 0.4f;
+		public const float DefaultBallBumpSpeedIncreaseFactor = 1.05f;
+		public const int DefaultBallSize = 40;
+		public const int WallDefaultSize = 100;
+		public const float MaxBallSpeed = 1.2f;
+		public const float PaddleYOffset = 50;
+	}
+}
